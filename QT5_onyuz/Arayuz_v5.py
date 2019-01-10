990961a2cdb54d4df2334de38e0bac3c76609bee
ysmn.akan3@gmail.com <ysmn.akan3@gmail.com>
2019-01-10 19:07:42 +0300

    Tweet atiliyor. Bagli kisi aliyor.

---
+++ QT5_onyuz/Arayuz_v5.py
@@ -0,0 +1,258 @@
+import sys
+import uuid
+import json
+
+sys.path.insert(0, '/home/yasemin/PycharmProjects/dagitik_home_group4-master/dagitik_home_group4')
+#sys.path.insert(0, '/home/mustafa/PycharmProjects/dagitik_home_group4')
+#sys.path.insert(0, '/home/mustafa/PycharmProjects/dagitik_home_group4')
+
+from PyQt5 import QtWidgets
+from Yayinci_Blogger import Yayinci_v4 as yay
+from QT5_onyuz.dagitik_proje_ui import Ui_MainWindow
+my_blog_list = []
+
+import threading
+import queue
+import socket
+import time
+
+
+class ProjectUi(QtWidgets.QMainWindow):
+    def __init__(self,logQueue):
+        self.logQueue=logQueue
+
+        self.qt_app = QtWidgets.QApplication(sys.argv)
+        QtWidgets.QWidget.__init__(self, None)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # list1 =[]
+        self.ui.connect_button.pressed.connect(self.connect)
+        # self.ui.connect_button.pressed.connect(self.get_host_with_port)
+        self.ui.connect_button.pressed.connect(self.change_profile_name)
+        # self.ui.connect_button.pressed.connect(self.disable_button)
+        self.ui.LogOut_button.pressed.connect(self.logout_button)
+        self.ui.Subscribe_button.pressed.connect(self.subscribe_button)
+        self.ui.UnSubscribe_button.pressed.connect(self.unsubscribe_button)
+        self.ui.UnBlock_button.pressed.connect(self.unblock_button)
+        self.ui.Block_button.pressed.connect(self.block_button)
+        self.ui.SendMessage_button.pressed.connect(self.send_message_button)
+        self.ui.Share_button.pressed.connect(self.share_twit_button)
+        self.ui.Pubkey_button.pressed.connect(self.pubkey_button)
+        self.initializeIpPort()
+        self.initializeMyBlogsList()
+
+        #self.list =QtWidgets.QListWidget(self)
+
+    def initializeIpPort(self):
+        self.ui.ip_field.setText("127.0.0.1")
+        self.ui.port_field.setText("12346")
+        self.ui.username_field.setText("Mustafa")
+        self.UUIDtoConnect=uuid.uuid4()
+
+    def initializeMyBlogsList(self):
+        try:
+            f_myBlogs = open('Mb.txt', 'r')
+            if f_myBlogs is None:
+                print("Error: Can not open f_myBlogs\n")
+            else:
+                print("Dosya acildi\n")
+                for line in f_myBlogs:
+                    yay.my_blog_list.append(line)
+            f_myBlogs.close()
+
+            for item in yay.my_blog_list:
+                self.ui.MyBlogList_field.addItem(item)
+        except FileNotFoundError:
+            pass
+
+
+
+
+    def connect(self):
+        self.get_host_with_port()
+        pass
+
+    def pubkey_button(self):
+        # pubkey buttonuna basıldığında gerçekleştirilecek eylem
+        request="PBKEY:"+"BUNUIMZALA"
+        # myClient = yay.ClientThread("Client Thread", self.UUIDtoConnect ,self.ip, self.port, request, self.logQueue)
+        myClient = yay.ClientThread("Client Thread", self.ip, self.port, request, self.logQueue)
+        response = myClient.control()
+        self.ui.SuggestedUser_field.addItem("XXX"+response+"XXX")
+        pass
+
+    def refresh_feed_button(self):
+        # takip edilen kişilerin serverlarına istek atarak twitleri yeniler
+        if(yay.BLOG == 1):
+            self.ui.Feeds_field.addItem(yay.my_mainpage)
+
+
+    def send_message_button(self):
+        # Send message to followed içine yazılan text yalnızca
+        # followers içinde check edilen kişilere gönderilecektir
+        pass
+
+    def block_button(self):
+        # Followers içindeki kullanıcılardan check edilenleri engelleyecektir.
+        pass
+
+    def unblock_button(self):
+        # Blocked içindeki kullanıcılardan check edilenlerin engellerini kaldıracaktır.
+        pass
+
+    def unsubscribe_button(self):
+        # Followed içindeki kullanıcılardan check edilenleri takipten çıkacaktır.
+        pass
+
+    def subscribe_button(self):
+        # Suggested users içindeki kullanıcılardan check edilenleri takip edecektir.
+        pass
+
+    def logout_button(self):
+        # logout ui kapatma olarak tasarlanmıştır. ileride connection close olarak değiştirilebilir
+        self.close()
+
+
+
+    def disable_button(self):
+        self.ui.connect_button.setDisabled(True)
+
+    def get_host_with_port(self):
+        # ip ve port alma işlemi
+        self.ip = self.ui.ip_field.text()
+        self.port = self.ui.port_field.text()
+        self.UUID ="AraciUUID"
+        yay.userInfoDict[self.UUID]=[ self.ip,self.port,"AraciName","NEGOTIATOR", None ]
+        with open('../Yayinci_Blogger/data.json', 'w') as fp:
+            json.dump(yay.userInfoDict, fp)
+
+        name=self.ui.username_field.text()
+        request="UINFO"
+        myClient=yay.ClientThread("Client Thread", self.ip, self.port, request, self.logQueue)
+        response=myClient.control()
+        self.ui.SuggestedUser_field.addItem("XXX" + response + "XXX")
+
+    def sendMyBlog(self):
+        request = "SBLOG:"+self.ui.username_field.text()+": "+self.ui.Twit_field.toPlainText()+"\n"
+        myClient = yay.ClientThread("Client Thread", self.ip, self.port, request, self.logQueue)
+        response = myClient.control()
+        self.ui.SuggestedUser_field.addItem("XXX" + response + "XXX")
+
+    def change_profile_name(self):
+        # kullanıcı adını bağlandığında otomatik olarak değiştirme
+        username = self.ui.username_field.text()
+        self.ui.UserNameLabel_field.setText(username)
+
+    def share_twit_button(self):
+        myBlog = self.ui.Twit_field.toPlainText()+"\n"
+        yay.my_blog_list.append(myBlog)
+        self.sendMyBlog()
+        f_myBlogs = open("Mb.txt", 'a')
+        f_myBlogs.write(myBlog)
+        f_myBlogs.close()
+        self.ui.MyBlogList_field.addItem(myBlog)
+        self.ui.Twit_field.clear()
+
+        # text = self.ui.Twit_field.toPlainText()
+        # request = text.strip()
+        # myClient = yay.ClientThread("Client Thread", self.UUIDtoConnect, self.ip, self.port, request, self.logQueue)
+        # response = myClient.control()
+        #
+        # self.ui.SuggestedUser_field.addItem("XXX" + response + "XXX")
+
+        '''
+        # share butonu ile my blog içerisine twit paylaşımı burası degisecek
+        text = self.ui.plainTextEdit_4.toPlainText()
+        if len(my_blog_list)==0:
+            notification = 'Henüz blog yazılmadı.'
+            my_blog_list.append(notification)
+            self.ui.listWidget_6.addItems(my_blog_list)
+        else:
+            my_blog_list.append(text)
+            self.ui.listWidget_6.addItems(my_blog_list)
+            # twit_list=[]
+            # twit1=self.ui.plainTextEdit_4.toPlainText()
+            # twit_list.append(twit1)
+            # self.ui.listWidget_6.addItems(twit_list) 
+        '''
+
+    def suggest_user(self, number_of_suggest, userlist):
+        # sayıya göre kullanıcı öneri listesini gösterme
+
+        # self.number_of_suggest = number_of_suggest
+        # self.userlist = userlist
+        # if userlist is None:
+        #    error_notification = 'Baglananan kullanıcı bulunmamaktadır.'
+        #    self.ui.listWidget_6.addItems(error_notification)
+        pass
+
+    def run(self):
+        self.show()
+        self.qt_app.exec_()
+
+
+class ArayuzThread(threading.Thread):
+    def __init__(self, threadname, logQueue):
+        threading.Thread.__init__(self)
+        self.threadName = threadname
+        self.logQueue = logQueue
+
+    def run(self):
+        app = ProjectUi(self.logQueue)
+        app.run()
+
+
+
+
+def main():
+    threads = []
+    # ana soket oluşturuluyor ve hangi iplerden bağlantı kabul edileceği, port numarası bilgileri giriliyor..
+    mySocket = socket.socket()  # Create a socket object
+    host = "0.0.0.0"  # Accesible by all of the network
+    port = yay.SERVER_PORT
+    mySocket.bind((host, port))  # Bind to the port
+
+    mySocket.listen(5)  # Now wait for client connection,
+    # with 5 queued connections at most
+
+    logQueue = queue.Queue()
+    exitFlag = False
+
+    Logger = yay.loggerThread("Logger", "log.txt", logQueue, exitFlag)
+    Logger.start()
+
+    userInputThread = yay.UserInputThread("User Input Thread", logQueue)
+    userInputThread.start()
+
+    arayUz = ArayuzThread("Arayüz Thread", logQueue)
+    arayUz.start()
+
+    # for i in range(2):
+    while True:
+        try:
+            log = "Waiting for connection from any client via port number " + str(port)
+            logQueue.put(time.ctime() + "\t\t - " + log)
+            print("Waiting for connection from any client via port number ", port)
+            c, addr = mySocket.accept()  # Establish connection with client. #blocking fonksiyon#c=soket,addr =adress of client
+            log = "Got connection from " + str(addr)
+            logQueue.put(time.ctime() + "\t\t - " + log)
+            print('Got connection from', addr)
+            serverThread = yay.ServerThread("Server Thread", c, yay.myUUID, yay.SERVER_HOST, yay.SERVER_PORT, yay.TYPE, logQueue, exitFlag)
+            serverThread.start()
+            threads.append(serverThread)
+
+
+        except KeyboardInterrupt:
+            break
+
+    #    clientThread = ClientThread("Client Thread")
+    #    clientThread.start()
+    #  threads.append(clientThread)
+
+    for t in threads:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Yayinci_Blogger/Yayinci_v5.py
@@ -0,0 +1,436 @@
+import threading
+import queue
+import socket
+import time
+import uuid
+import json
+# import sys
+
+#sys.path.insert(0, '/home/mustafa/PycharmProjects/dagitik_home_group4')
+#import QT5_onyuz.dagitik_proje_main
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA256
+
+
+
+random_generator = Random.new().read
+f_priv = open('id_rsa','w+')
+f_pub = open('id_rsa.pub','w+')
+
+if(f_priv and f_pub):
+    my_private_key = f_priv.read()
+    my_public_key = f_pub.read()
+    if(my_private_key == ""):
+        new_key = RSA.generate(1024, randfunc=random_generator)
+        my_public_key = new_key.publickey()
+        f_pub.write(str(my_public_key.exportKey().decode()))
+        print(my_public_key.exportKey())
+        my_private_key = new_key
+        f_priv.write(str(my_private_key.exportKey().decode()))
+        #print(new_key.exportKey())
+
+f_priv.close()
+f_pub.close()
+
+my_blog_list = []
+my_mainpage = []
+BLOG = 0
+
+
+STATUS = 0
+
+
+THREADNUM = 5
+CONNECT_POINT_LIST = []  # list array of [ip,port,type,time]
+SERVER_PORT  = 12345
+SERVER_PORT2 = 12346
+# SERVER_HOST = socket.gethostbyname(socket.gethostname())
+SERVER_HOST = "127.0.0.1"
+TYPE = "NEGOTIATOR"
+
+NUMBER_OF_USERLIST = 5
+NAME = "MUSTAFA"
+
+serverQueue = queue.Queue()
+clientQueue = queue.Queue()
+
+# uuid.NAMESPACE_DNS.hex # dagdelenin metodu(MAC E göre)
+
+
+userInfoDict = dict()
+try:
+    with open('data.json', 'r') as fp:
+        userInfoDict = json.load(fp)
+except FileNotFoundError:
+    with open('data.json', 'w') as fp:
+        json.dump(userInfoDict, fp)
+
+myUUID = uuid.uuid4()
+print("my UUID = " + str(myUUID))
+
+
+# tmpUUID="yasemin"
+# userInfoDict[tmpUUID]=["yadress", "yPort", "yNanme", "yNEGOTIATOR",None]
+# tmpUUID="orhan"
+# userInfoDict[tmpUUID]=["oadress", "oPort", "oNanme", "oNEGOTIATOR",None]
+# tmpUUID="a"
+# userInfoDict[tmpUUID]=["4", "4", "4", "4",None]
+# with open('data.json', 'w') as fp:
+#     json.dump(userInfoDict, fp)
+
+
+# loglama işlemini yapacak thread tanımlanıyor.
+class loggerThread(threading.Thread):
+    def __init__(self, threadName, logFilePath, logQueue, exitFlag):
+        threading.Thread.__init__(self)
+        self.threadName = threadName
+        self.logQueue = logQueue
+        self.logFilePath = logFilePath
+        self.exitFlag = exitFlag
+
+    def run(self):
+        file = open(self.logFilePath, 'a')
+        file.write(str(time.ctime()) + " - " + self.threadName + " starting.\n")
+        while not self.exitFlag:
+            if not self.logQueue.empty():
+                log = self.logQueue.get()
+                if log == "QUITT":
+                    self.exitFlag = True
+                    file.write(str(time.ctime()) + " - " + "Logger thread : OUITT received." + "\n")
+                else:
+                    file.write(str(time.ctime()) + " - " + str(log) + "\n")
+        file.write(str(time.ctime()) + " - " + self.name + "exiting." + "\n")
+        file.close()
+
+
+class ServerThread(threading.Thread):
+    def __init__(self, threadName, mySocket, myHostname, myPort, myName, myType, logQueue, exitFlag):
+        threading.Thread.__init__(self)
+        self.threadName = threadName
+        self.mySocket = mySocket
+        self.myHostname = myHostname
+        self.myPort = myPort
+        self.myName = myName
+        self.myType = myType
+        self.logQueue = logQueue
+        self.exitFlag = exitFlag
+
+    def run(self):
+        log = "Server thread starting."
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        while True:
+            msgReiceved = ((self.mySocket.recv(1024)).decode()).strip()
+            if len(msgReiceved) > 1:
+                msgToSend = ""
+                print("ServerThread:AlinanMesaj=" + msgReiceved)
+                log = "Server thread received a message : " + msgReiceved
+                self.logQueue.put(time.ctime() + "\t\t - " + log)
+                msgToSend = str(self.readerParser(msgReiceved))
+                if len(msgToSend) >= 5:
+                    print("ServerThread:GönderilenMesaj=" + msgToSend)
+                    self.mySocket.send((msgToSend).encode())
+                    log = "Server thread sent a message : " + msgToSend
+                    self.logQueue.put(time.ctime() + "\t\t - " + log)
+
+    def readerParser(self, request):
+        prot = request[:5]
+        response = ""
+        global STATUS
+        global userInfoDict
+
+        if prot == "HELLO":
+            response = "HELLO"
+
+        elif prot == "UINFO":  # YENI KULLANICI İSTEĞİ /KULLANICI BAĞLANTI KONTROLU
+            paramIndex = request.find(":")
+            # print("UINFODAYIM= " + str(paramIndex))
+            if (not paramIndex == -1):
+                # print("paramiGectimBenreaderPArserim " + request + " ServerReaderThread")
+                paramList = ((request[paramIndex + 1:]).strip()).split('$')
+                # print("paramList[0]= "+ str(paramList[0]))
+                #with open('data.json', 'r') as fp:
+                 #   userInfoDict = json.load(fp)
+
+                if paramList[0] in userInfoDict.keys():
+                    response = "CHKED"
+                # userInfoList[paramList[0]]=list(paramList[1],paramList[2],paramList[3],paramList[4])
+                else:  # BEKLE AZ SEN
+                    UUIDtoCheck = paramList[0]
+                    # clientQueue.put("CHECK")
+                    myClient = ClientThread("Client Thread", paramList[1], int(paramList[2]), "CHECK", self.logQueue)
+                    response = myClient.control()
+                    # print('RESPONSE:' + response)
+                    # print('UUID:' + UUIDtoCheck)
+                    if str(response[6:]) == str(UUIDtoCheck):
+                        msg = "CONOK"
+                        STATUS=1
+                        #with open('data.json', 'r') as fp:
+                         #   userInfoDict = json.load(fp)
+
+                        userInfoDict[paramList[0]] = [paramList[1], paramList[2], paramList[3], paramList[4],None]
+
+                        with open('data.json', 'w') as fp:
+                            json.dump(userInfoDict, fp)
+                    else:
+                        msg = "CONER"
+
+                    print("ServerThread:GönderilenMesaj=" + msg)
+                    self.mySocket.send(((msg).strip()).encode())
+                    log = "Server thread : " + "UUIDs checked.   " + msg
+                    self.logQueue.put(time.ctime() + "\t\t - " + log)
+                    msgReiceved = ((self.mySocket.recv(1024)).decode()).strip()
+                    if len(msgReiceved) > 1:
+                        print("ServerThread:AlinanMesaj=" + msgReiceved)
+                        pass
+
+
+            else:
+                response = "ERROR"
+
+
+        elif prot == "CHECK":  # UUID KONTROLU
+            response = "MUUID" + ":" + str(myUUID)
+
+        elif prot == "CONOK":  # INF :UUID TUTARLI
+            response = "HELLO"
+
+        elif prot == "CONER":  # INF: UUID FARKLI
+            response = "BYBYE"
+
+        elif prot == "LSUSR":  # KULLANICI LISTE PAYLASIMI
+            # paramIndex=request.find(":")
+            # if(not paramIndex == -1):
+            #     nbUser=( request[paramIndex+1:] ).strip()
+            i = 0
+            paramList = ""
+            #with open('data.json', 'r') as fp:
+             #   userInfoDict = json.load(fp)
+
+            for key in userInfoDict.keys():
+                if i < NUMBER_OF_USERLIST:
+                    param = key + "," + userInfoDict.get(key)[0] + "," + userInfoDict.get(key)[1] + "," + \
+                            userInfoDict.get(key)[2] + "," + userInfoDict.get(key)[3]
+                    # print("PARAMMMMMMM"+param)
+                    paramList += param + "$"
+                else:
+                    break
+                i += 1
+            paramList = paramList[:-1]
+            response = "LSUOK" + ":" + paramList
+        elif prot == "QUITT":
+            response = "EXITT"
+            exitFlag = True
+            self.logQueue.put("QUITT")
+
+
+
+        ##BURDAN ITIBAREN YAYINCI ICIN OLANLAR
+        elif prot == "PBKEY":  # public_key paylasimi
+            if (STATUS == 0):
+                response = "CFAIL"
+            else:
+                paramIndex = request.find(":")
+                # print("UINFODAYIM= " + str(paramIndex))
+                if (not paramIndex == -1):
+                    # print("paramiGectimBenreaderPArserim " + request + " ServerReaderThread")
+                    paramList = ((request[paramIndex + 1:]).strip()).split('$')
+                    msgToSign=paramList[0]
+                    hash = SHA256.new(msgToSign.encode()).digest()
+                    print(hash)
+                    signature = my_private_key.sign(hash, '')
+                    msgToSend = "MYPUB:" + str( (my_public_key.exportKey()).decode() ) + '$' + str(signature)
+                    pkstring = my_public_key.exportKey()
+                    #pkstring = my_public_key
+                    print(pkstring)
+                    ####msgToSend = "MYPUB:".encode() + pkstring
+                    #msgToSend = "MYPUB:" + str(pkstring) + '$' + str(signature)
+                    #print("MYPUBBBB  "+msgToSend)
+                    #myClient = ClientThread("Client Thread", SERVER_HOST, int(SERVER_PORT), msgToSend, self.logQueue)
+                    #response = myClient.control()
+                    self.mySocket.send(msgToSend.encode())
+                    #myClient = ClientThread("Client Thread", SERVER_HOST, int(SERVER_PORT), "MYPUB", self.logQueue)
+                    #response = myClient.publicKey_control()
+
+                    if response[:5] == "PUBOK":
+                        print("PUBOK GELDİİ")
+                    elif response[:5] =="PUBER":
+                        print("PUBER GELDİİ")
+                    response=""
+
+                else:
+                    response = "ERROR"
+
+
+        ##BURADAN ITIBAREN SIFRELILER
+        elif prot == "SNDMB":
+            if (STATUS == 0):
+                response = "CFAIL"
+            else:
+                numberOfBlogs = int(request[7:])
+                response =  "MYMBS:"+my_blog_list[0:numberOfBlogs]
+
+        elif prot == "SBLOG":
+            if (STATUS == 0):
+                response = "CFAIL"
+            else:
+                my_mainpage.append(request[7:])
+                print("MAINNNNNNNNNN: ")
+                print(my_mainpage)
+                response = "BLGOK"
+
+        else:
+            response = "ERROR"
+        log = "Server thread : " + response
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        return response
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, threadName, hostToConnect, portToConnect, cmnd, logQueue):
+        threading.Thread.__init__(self)
+        self.threadName = threadName
+        self.hostToConnect = hostToConnect
+        self.portToConnect = portToConnect
+        self.cmnd = cmnd
+        self.logQueue = logQueue
+
+    def run(self):
+        pass
+
+    def control(self):
+        global BLOG
+        global userInfoDict
+        log = self.threadName + " : " + "is controlling."
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        mySocket = socket.socket()  # Create a socket object
+        # host = "192.168.1.107"  # Connection point (localhost)
+        host = self.hostToConnect  # Connection point (localhost)
+        # port = SERVER_PORT  # Reserve a port for your service.
+        port = self.portToConnect  # Reserve a port for your service.
+
+        mySocket.connect((host, int(port)))
+
+        prot = self.cmnd[:5]
+        if prot == "UINFO":
+            paramList = str(myUUID) + "$" + SERVER_HOST + "$" + str(SERVER_PORT) + "$" + NAME + "$" + TYPE
+            textToSend = prot + ":" + paramList
+        else:
+            textToSend = self.cmnd
+
+        log = self.threadName + " : " + "sending a message : " + textToSend
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        # print(s.recv(1024).decode())  # blocking'dir
+        #print("ClientThread:GönderilenMesaj=" + textToSend)
+        mySocket.send((textToSend.strip()).encode())
+        response = ((mySocket.recv(1024)).decode()).strip()
+        print(type(response))
+        #print("ClientThread:AlinanMesaj=" + response)
+        mySocket.close()  # Close the socket when done
+
+
+        if(response[:5] == "MYPUB"):
+            pub_sifrelimesaj=response[6:].strip().split("$")
+            print(type(pub_sifrelimesaj[0]))
+            print("PKKK = " + pub_sifrelimesaj[0])
+            print("sifreli mesaj = " + pub_sifrelimesaj[1])
+            pubkey = RSA.importKey((pub_sifrelimesaj[0]).encode())
+            hash= SHA256.new("BUNUIMZALA".encode()).digest()
+            print(hash)
+            print((pub_sifrelimesaj[1]).encode())
+            signature = int(pub_sifrelimesaj[1][1:-1].split(",")[0])
+            signature = (signature, '')
+            is_verified = pubkey.verify(hash,signature)
+            #is_verified = key.verify(hash,(pub_sifrelimesaj[1]).encode())
+            #with open('data.json', 'r') as fp:
+             #   userInfoDictt = json.load(fp)
+            if(is_verified == True):
+                for k in userInfoDict.keys():
+                    print("KKKKKKK")
+                    print((userInfoDict[k])[0])
+                    if (userInfoDict[k])[0]==self.hostToConnect and (userInfoDict[k])[1]==self.portToConnect:
+                        print("KEYI ATAMAM LAZIM")
+                        (userInfoDict[k])[4] = str(pubkey.exportKey())
+                        with open('data.json', 'w') as fp:
+                            json.dump(userInfoDict, fp)
+                response = "PUBOK"
+            else:
+                response = "PUBER"
+            #pubkey = key.publickey()
+            #print("PUBLIIICCC "+key)
+
+        if(response[:5] == "BLGOK"):
+            BLOG = 1
+            #my_mainpage.append(self.cmnd[7:])
+
+
+        if (response[:5] == "LSUOK"):
+            pass
+
+        if len(response) > 1:
+            # print("ClientThread " + response + " ClientThread")
+            log = self.threadName + " : " + "response is " + response
+            self.logQueue.put(time.ctime() + "\t\t - " + log)
+            return response
+        log = self.threadName + " : " + "response is " + ""
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        return ""
+
+class UserInputThread(threading.Thread):
+    def __init__(self, threadname, logQueue):
+        threading.Thread.__init__(self)
+        self.threadName = threadname
+        self.logQueue = logQueue
+
+    def run(self):
+        log = self.threadName + " : " + "is starting."
+        self.logQueue.put(time.ctime() + "\t\t - " + log)
+        while True:
+            request = input("Enter your request > ")
+            log = self.threadName + " : " + "Input request is " + request
+            self.logQueue.put(time.ctime() + "\t\t - " + log)
+            myClient = ClientThread("Client Thread", SERVER_HOST, SERVER_PORT, request, self.logQueue)
+            response = myClient.control()
+
+
+def main():
+    threads = []
+    # ana soket oluşturuluyor ve hangi iplerden bağlantı kabul edileceği, port numarası bilgileri giriliyor..
+    mySocket = socket.socket()  # Create a socket object
+    host = "0.0.0.0"  # Accesible by all of the network
+    port = SERVER_PORT
+    mySocket.bind((host, port))  # Bind to the port
+
+    mySocket.listen(5)  # Now wait for client connection,
+    # with 5 queued connections at most
+
+    logQueue = queue.Queue()
+    exitFlag = False
+
+    Logger = loggerThread("Logger", "log.txt", logQueue, exitFlag)
+    Logger.start()
+
+    userInputThread = UserInputThread("User Input Thread", logQueue)
+    userInputThread.start()
+
+    while True:
+        try:
+            log = "Waiting for connection from any client via port number " + str(port)
+            logQueue.put(time.ctime() + "\t\t - " + log)
+            print("Waiting for connection from any client via port number ", port)
+            c, addr = mySocket.accept()  # Establish connection with client. #blocking fonksiyon#c=soket,addr =adress of client
+            log = "Got connection from " + str(addr)
+            logQueue.put(time.ctime() + "\t\t - " + log)
+            print('Got connection from', addr)
+            serverThread = ServerThread("Server Thread", c, myUUID, SERVER_HOST, SERVER_PORT, TYPE, logQueue, exitFlag)
+            serverThread.start()
+            threads.append(serverThread)
+        except KeyboardInterrupt:
+            break
+
+    for t in threads:
+        t.join()
+
+if __name__ == '__main__':
+    main()
